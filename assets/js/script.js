5d63b7ee0e94d028466b1d0f29b221f3460a1ac8
Ali <alissaeid@live.co.uk>
2023-02-13 20:09:58 +0000

    Add Basic sturcture, CSS Styling, images, js functionality, css responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+//**navbar**//
+
+//Select the button element
+let button = document.querySelector(".navbar-toggler");
+
+//Select the menu element
+let menu = document.querySelector(".navbar-collapse");
+
+//Add a click event listener to the button element
+button.addEventListener("click", function () {
+  //Toggle the "collapse" class on the menu element
+  menu.classList.toggle("collapse");
+});
+
+//**smooth scrolling**//
+
+// Select all links with "#" href
+let links = document.querySelectorAll('a[href^="#"]');
+
+// Iterate through the links
+for (let i = 0; i < links.length; i++) {
+  let link = links[i];
+
+  // Add click event listener to each link
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Get the target element's id
+    let targetId = this.getAttribute("href");
+    let target = document.querySelector(targetId);
+
+    // Animate the scroll to the target element
+    let scrollOptions = {
+      left: 0,
+      top: target.getBoundingClientRect().top + window.pageYOffset,
+      behavior: "smooth"
+    };
+    window.scrollTo(scrollOptions);
+  });
+}
+
+//Getting game elements
+
+const section = document.querySelector('section');
+const lives = document.querySelector('span');
+const playerLives = 6;
